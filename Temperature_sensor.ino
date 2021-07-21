c476ca5444159fb9fd6a81aaf2a0cedfc74924b4
Michał Dołharz <michal@atu-reklama.pl>
2021-07-21 21:49:09 +0200

    Added headers to the csv files and refactorized

--- Temperature_sensor.ino
+++ Temperature_sensor.ino
@@ -3,7 +3,7 @@
 #include <SD.h>
 
 #define LM35 A4 
-#define interval 5UL // seconds (UL is needed for compiler to make it unsigned long type)
+#define interval 1UL // minutes (UL is needed for compiler to make it unsigned long type)
 
 File SDCardFile; // a file into which data will be written 
 virtuabotixRTC RTC(6, 7, 8); // a RTC 
@@ -18,17 +18,64 @@ void setup(){
 
   // Set a current time to the RTC [seconds, minutes, hours, dayofweek, dayofmonth, month, year]
   //               (ss, mm, hh, w, dd, mm, yyyy) w - day of week 
-  RTC.setDS1302Time(00, 01, 21, 3, 21, 07, 2021);
+  RTC.setDS1302Time(00, 22, 21, 3, 21, 07, 2021);
 
   // Initialize an SD card
+  SD_init();
+
+  // Create file and make headers
+  CreateFile();
+
+  // Initial data save  
+  CollectData();
+}
+
+
+void loop(){
+
+  // Check the time
+  timeNow = millis();
+  
+  if(timeNow - timestamp >= interval * 60 * 1000)
+  {
+    // Save the new timestamp
+    timestamp = timeNow;
+
+    // Save data
+    CollectData();
+  }
+}
+
+void CollectData()
+{
+  // Update user RTC time variables
+  RTC.updateTime();
+   
+  // Measure the temperature
+  float temp = ((analogRead(LM35) * 5.0) / 1024.0) * 100;
+
+  // Save data on SD card
+  SaveDataOnSDCard(RTC, temp);
+
+  // Send info to UART
+  Info_UART(RTC, temp);
+}
+
+void SD_init()
+{
   if (!SD.begin(10)) {
     Serial.println("SD card initialization failed!");
     while (1);
   }
+}
 
+void CreateFile()
+{
   // Search for non existing filename
   while(SD.exists(filename))
   {
+    Serial.print("File exists: ");
+    Serial.println(filename);
     fileNum++;
     sprintf(filename, "Temp%02d.csv", fileNum);
   }
@@ -62,31 +109,6 @@ void setup(){
   {
     Serial.print("Failed to open file: ");
     Serial.println(filename);
-  }
-}
-
-
-void loop(){
-
-  // Check the time
-  timeNow = millis();
-  
-  if(timeNow - timestamp >= interval * 0.2 * 1000)
-  {
-    // Save the new timestamp
-    timestamp = timeNow;
-
-    // Update user RTC time variables
-    RTC.updateTime();
-    
-    // Measure the temperature
-    float temp = ((analogRead(LM35) * 5.0) / 1024.0) * 100;
-
-    // Save data on SD card
-    SaveDataOnSDCard(RTC, temp);
-
-    // Send info to UART
-    Info_UART(RTC, temp);
   }
 }
 
