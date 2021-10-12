c2b3727a14d4ccc582c936c45dd41a64f2d516eb
Michał Dołharz <michal@atu-reklama.pl>
2021-10-12 21:34:33 +0200

    Optional seconds saving added

--- Temperature_sensor.ino
+++ Temperature_sensor.ino
@@ -2,27 +2,32 @@
 #include <SPI.h>
 #include <SD.h>
 
+#define LED 3
 #define LM35 A4 
 #define RTC_RST 6 // CS
 #define RTC_DAT 7 // MISO
 #define RTC_CLK 8 // SCK
-#define interval 1UL // minutes (UL is needed for compiler to make it unsigned long type)
+#define interval 5UL // minutes (UL is needed for compiler to make it unsigned long type)
 
 File SDCardFile; // a file into which data will be written 
-virtuabotixRTC RTC(RTC_RST, RTC_DAT, RTC_CLK); // a RTC 
+virtuabotixRTC RTC( RTC_CLK, RTC_DAT, RTC_RST); // a RTC 
 unsigned long timestamp = 0; // a variable to set a timestamp to measure a time between temperature measuments
 unsigned long timeNow; // a variable to measure a current time
-int fileNum = 0;
-char filename[] = "Temp00.csv";
+int fileNum = 0; // file number
+char filename[] = "Tem000.csv"; // first file name
+bool seconds_OnOff_flag = false; // flag for turning on (true) or off {false} saving seconds
 
 void setup(){
+pinMode(LED, OUTPUT);
+  
   // UART
-  Serial.begin(9600);
+  //Serial.begin(9600);
 
-  // Set a current time to the RTC [seconds, minutes, hours, dayofweek, dayofmonth, month, year]
-  //               (ss, mm, hh, w, dd, mm, yyyy) w - day of week 
-  RTC.setDS1302Time(00, 22, 21, 3, 21, 07, 2021);
+   //Set a current time to the RTC [seconds, minutes, hours, dayofweek, dayofmonth, month, year]
+   //                               (ss, mm, hh, w, dd, mm, yyyy) w - day of week 
+   //RTC.setDS1302Time(00, 8, 21, 2, 12, 10, 2021);
 
+delay(200);
   // Initialize an SD card
   SD_init();
 
@@ -35,7 +40,7 @@ void setup(){
 
 
 void loop(){
-
+//digitalWrite(LED, HIGH);
   // Check the time
   timeNow = millis();
   
@@ -61,15 +66,17 @@ void CollectData()
   SaveDataOnSDCard(RTC, temp);
 
   // Send info to UART
-  Info_UART(RTC, temp);
+  //Info_UART(RTC, temp);
 }
 
 void SD_init()
 {
   if (!SD.begin(10)) {
-    Serial.println("SD card initialization failed!");
-    while (1);
+    //Serial.println("SD card initialization failed!");
+    error(500);
   }
+  else
+    blinkLEDStart();
 }
 
 void CreateFile()
@@ -77,15 +84,23 @@ void CreateFile()
   // Search for non existing filename
   while(SD.exists(filename))
   {
-    Serial.print("File exists: ");
-    Serial.println(filename);
+    if(fileNum >= 1000)
+    {
+      //digitalWrite(LED, HIGH);
+      //while(1){}
+      sprintf(filename, "TemRES.csv", fileNum);
+      break;
+    }
+    
+    //Serial.print("File exists: ");
+    //Serial.println(filename);
     fileNum++;
-    sprintf(filename, "Temp%02d.csv", fileNum);
+    sprintf(filename, "Tem%03d.csv", fileNum);
+    //sprintf(filename, "Tem0.csv");
   }
-  
   // Info to UART
-  Serial.print("File name: ");
-  Serial.println(filename);
+  //Serial.print("File name: ");
+  //Serial.println(filename);
 
   // Open the file to write headers
   SDCardFile = SD.open(filename, FILE_WRITE);
@@ -103,15 +118,23 @@ void CreateFile()
     SDCardFile.print(",");
     SDCardFile.print("Minute");
     SDCardFile.print(",");
+    if(seconds_OnOff_flag)
+    {
+      SDCardFile.print("Seconds");
+      SDCardFile.print(",");
+    }
     SDCardFile.println("Temperature");
     
     // Close the file
     SDCardFile.close();
+
+    blinkLED();
   }
   else
   {
-    Serial.print("Failed to open file: ");
-    Serial.println(filename);
+    //Serial.print("Failed to open file: ");
+    //Serial.println(filename);
+    error(35);
   }
 }
 
@@ -134,21 +157,29 @@ void SaveDataOnSDCard(virtuabotixRTC RTC, float temp)
     SDCardFile.print(",");
     SDCardFile.print(RTC.minutes);
     SDCardFile.print(",");
+    if(seconds_OnOff_flag)
+    {
+      SDCardFile.print(RTC.seconds);
+      SDCardFile.print(",");
+    }
     SDCardFile.println(temp);
     
     // Close the file
     SDCardFile.close();
+
+    blinkLED();
   }
   else
   {
-    Serial.print("Failed to open file: ");
-    Serial.println(filename);
+    //Serial.print("Failed to open file: ");
+    //Serial.println(filename);
+    //error(100);
   }
   
 }
 
 // Send info to UART
-void Info_UART(virtuabotixRTC RTC, float temp)
+/*void Info_UART(virtuabotixRTC RTC, float temp)
 {
   Serial.print(RTC.year);
   Serial.print(",");
@@ -160,5 +191,37 @@ void Info_UART(virtuabotixRTC RTC, float temp)
   Serial.print(",");
   Serial.print(RTC.minutes);
   Serial.print(",");
+  Serial.print(RTC.seconds);
+  Serial.print(",");
   Serial.println(temp);
+}*/
+
+void blinkLED()
+{
+  digitalWrite(LED, HIGH);
+  delay(200);
+  digitalWrite(LED, LOW);
+
+}
+
+void blinkLEDStart()
+{
+  for(int i =0; i <=10; i++){
+  digitalWrite(LED, HIGH);
+  delay(100);
+  digitalWrite(LED, LOW);
+  delay(100);
+}
+
+}
+
+void error(int t)
+{
+    while(1)
+    {
+      digitalWrite(LED, HIGH);
+      delay(t);
+      digitalWrite(LED, LOW);
+      delay(t);
+    }
 }
